1bfa41846b1e74d3b7aab4d5189939a37397fe00
Vasili Utenkov <vasili.utenkov@yandex.ru>
2025-03-05 00:17:09 +0300

    Код

--- test/manager/file/FileBackedTaskManagerTest.java
+++ test/manager/file/FileBackedTaskManagerTest.java
@@ -1,24 +1,14 @@
 package manager.file;
 
-import manager.Managers;
-import manager.TaskManager;
-import manager.TaskManagerTest;
-import manager.file.FileBackedTaskManager;
-import org.junit.jupiter.api.*;
-import tasks.Epic;
-import tasks.Status;
-import tasks.SubTask;
-import tasks.Task;
 
+import manager.TaskManagerTest;
+import org.junit.jupiter.api.*;
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
-
-    //taskManager = new FileBackedTaskManager();
 
     public File savesTasks = new File("src/Tasks.csv");
     private final FileBackedTaskManager taskManager = new FileBackedTaskManager(savesTasks);
@@ -47,41 +37,52 @@ class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
 
     @Test
     void loadFromFile() {
-        taskManager.loadTasksFromFile(savesTasks);
 
-        taskManager.getTasksList();
+        int taskCount = taskManager.getTasksList().size();
+        assertEquals(2, taskCount, "Количество загруженных задач не совпадает"
+                + " с сохраненными в файле. Ожидалось 2, загрузилось " + taskCount);
 
-        taskManager.getEpicsList();
+        int epicCount = taskManager.getEpicsList().size();
+        assertEquals(1, epicCount,  "Количество загруженных эписов не совпадает"
+                + " с сохраненными в файле. Ожидалось 1, загрузилось "+ epicCount);
 
-        taskManager.getSubTasksList();
-
+        int subTaskCount = taskManager.getSubTasksList().size();
+        assertEquals(3, subTaskCount, "Количество загруженных подзадач не совпадает"
+                + " с сохраненными в файле. Ожидалось 3, загрузилось " + subTaskCount);
     }
 
 
 //
 // Иначе получал "Cannot invoke "manager.TaskManager.getEpicsList()" because "this.taskManager" is null"
-// Готов выслушать напставления и инструкции как с этим бороться
+// Готов выслушать наставления и инструкции как с этим бороться
 //
 
     @Test
-    void getSubTasks() {    }
+    void getSubTasks() {
+    }
 
     @Test
-    void getTasks() {    }
+    void getTasks() {
+    }
 
     @Test
-    void getEpicsList() {    }
+    void getEpicsList() {
+    }
 
     @Test
-    void checkEpicStatusWhenAllInNew(){    }
+    void checkEpicStatusWhenAllInNew() {
+    }
 
     @Test
-    void checkEpicStatusWhenAllInDone(){    }
+    void checkEpicStatusWhenAllInDone() {
+    }
 
     @Test
-    void checkEpicStatusWhenMixedNewAndDone(){    }
+    void checkEpicStatusWhenMixedNewAndDone() {
+    }
 
     @Test
-    void checkEpicStatusWhenAllInProgress(){    }
+    void checkEpicStatusWhenAllInProgress() {
+    }
 
 }
