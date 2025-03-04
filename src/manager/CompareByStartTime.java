152c3e5be7d9d42ee7db6f64e1afef13575b0506
Vasili Utenkov <vasili.utenkov@yandex.ru>
2025-03-04 03:40:24 +0300

    Код

---
+++ src/manager/CompareByStartTime.java
@@ -0,0 +1,19 @@
+package manager;
+
+import tasks.Task;
+
+import java.util.Comparator;
+
+public class CompareByStartTime implements Comparator<Task> {
+    @Override
+    public int compare(Task task1, Task task2) {
+        if (task1.getStartTime().isPresent() && task2.getStartTime().isPresent()) {
+            if (task1.getStartTime().get().isAfter(task2.getStartTime().get())) {
+                return 1;
+            } else if ((task1.getStartTime().get().isBefore(task2.getStartTime().get()))) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+}
