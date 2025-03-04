5fa2d8e10322ffb8a1b17ce247f1933c4134b8cf
Vasili Utenkov <vasili.utenkov@yandex.ru>
2025-03-04 20:37:13 +0300

    Код

--- test/manager/file/FileBackedTaskManagerTest.java
+++ test/manager/file/FileBackedTaskManagerTest.java
@@ -17,11 +17,10 @@ class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
 
     public static File savesTasks;
 
-
+/*
     static {
         try {
             savesTasks = File.createTempFile("Tasks", ".tmp", new File("D:\\JavaCourse"));
-//                savesHistory = File.createTempFile("History", "tmp", new File("D:\\JavaCourse"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -31,51 +30,23 @@ class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
     public void setUp () throws IOException {
         savesTasks = File.createTempFile("Tasks", "tmp", new File("D:\\JavaCourse"));
         taskManager = new FileBackedTaskManager(savesTasks);
-//        savesHistory = File.createTempFile("History", "tmp", new File("D:\\JavaCourse"));
-//        taskManager.setFiles(savesTasks);
         initTask();
     }
 
     @AfterEach
     public void deleteFile () {
         savesTasks.deleteOnExit();
-//        savesHistory.deleteOnExit();
     }
 
-
-        //final TaskManager taskManager = Managers.getDefault(savesTasks);
-
-
-//    private final TaskManager taskManager = Managers.getDefault();
-//    private final HistoryManager historyManager = Managers.getDefaultHistory();
-
-
-/*
-        @BeforeEach
-        public void setUp () throws IOException {
-        savesTasks = File.createTempFile("Tasks", "tmp", new File("D:\\JavaCourse"));
-        taskManager = new FileBackedTaskManager(savesTasks);
-//        savesHistory = File.createTempFile("History", "tmp", new File("D:\\JavaCourse"));
-//        taskManager.setFiles(savesTasks);
-        initTask();
-    }
-
-        @AfterEach
-        public void deleteFile () {
-        savesTasks.deleteOnExit();
-//        savesHistory.deleteOnExit();
-    }
 */
 
-
-/*
     @BeforeEach
     public void setUp() {
-        file = new File("test_" + System.nanoTime() + ".csv");
-        taskManager = new FileBackedTaskManager(file);
+        savesTasks = new File("src/Tasks.csv");
+        taskManager = new FileBackedTaskManager(savesTasks);
         initTask();
     }
-*/
+
 
         @AfterEach
         void tearDown () {
